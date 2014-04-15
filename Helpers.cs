fcbe076eb86987bbbcb4aa7c9ab6b7d53c9d79e4
Jan Mechtel <jmechtel@gmail.com>
2014-04-15 22:45:14 +0200

    trying to change the warning text of the pause button

---
+++ Helpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace SitPerfect
+{
+    //http://stackoverflow.com/questions/841808/wpf-display-a-bool-value-as-yes-no
+    public class BoolToStringConverter : BoolToValueConverter<String> { }
+    
+    public class BoolToValueConverter<T> : IValueConverter
+    {
+        public T FalseValue { get; set; }
+        public T TrueValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return FalseValue;
+            else
+                return (bool)value ? TrueValue : FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value != null ? value.Equals(TrueValue) : false;
+        }
+    }
+
+}
